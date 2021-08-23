ca69e56967b58bb4d2d7fe6ab41994ee6a235824
FerhatKavakli <ferhatkavakli@hotmail.com>
2021-08-23 22:12:48 +0200

    home page methods

--- src/test/java/com/nextBase/pages/HomePage.java
+++ src/test/java/com/nextBase/pages/HomePage.java
@@ -1,6 +1,26 @@
 package com.nextBase.pages;
 
+import com.nextBase.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage extends BasePage {
 
-    //commit
+    public HomePage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+    public void navigateToMenu(String menu){
+        Driver.get().findElement(By.xpath("//span[@class='menu-item-link-text' and contains(text(), '" + menu + "')]")).click();
+    }
+
+    public void navigateToTab(String tab){
+        Driver.get().findElement(By.xpath("//span[@class='main-buttons-item-text-title' and contains(text(), '"+tab+"')]")).click();
+    }
+    @FindBy(css = ".pagetitle-flexible-space")
+    public WebElement tasksContainer;
+
+    @FindBy(xpath = "//div[@class='main-ui-square-item'][2]")
+    public WebElement ongoing;
 }
