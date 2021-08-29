552778438d08e3cf92c913856b5526c37b12eed1
MerttSari <Sari.mertt.99@gmail.com>
2021-08-29 18:00:34 +0300

    update

--- src/test/java/com/nextBase/stepDefinitions/TasksStepDef.java
+++ src/test/java/com/nextBase/stepDefinitions/TasksStepDef.java
@@ -47,17 +47,81 @@ public class TasksStepDef {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
-    @When("user click ADD TASK button")
-    public void user_click_ADD_TASK_button() {
+    @When("user click {string} button")
+    public void user_click_button(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Then("user should be able to add new task")
     public void user_should_be_able_to_add_new_task() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
+
+
+
+
+    @When("user click created task")
+    public void user_click_created_task() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user click EDIT")
+    public void user_click_EDIT() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should be able to edit the task")
+    public void user_should_be_able_to_edit_the_task() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+
+    @When("user click More")
+    public void user_click_More() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user add Time tracking")
+    public void user_add_Time_tracking() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user add reminder")
+    public void user_add_reminder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user add Dependent tasks")
+    public void user_add_Dependent_tasks() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user can see edited following informations")
+    public void user_can_see_edited_following_informations(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
 
 
 }
--- src/test/resources/features/Tasks.feature
+++ src/test/resources/features/Tasks.feature
@@ -1,14 +1,40 @@
 Feature: Tasks page functionality
-@wip
+
   Scenario: HR User can create task
     Given user already logged in with "hr1@cybertek.com" username "UserUser" password
     When user click "Tasks" "All" under Activity stream
     And user click NEW TASK button
     And user enter following informations
-    |Things to do|Test|
-    |Description |Testing this page|
+      | Things to do | Test              |
+      | Description  | Testing this page |
     And user click High Priority check box
     And user enter deadline
     And user Add mention
-    And  user click ADD TASK button
+    And  user click "ADD TASK" button
     Then user should be able to add new task
+
+
+  Scenario: Marketing User can edit the task
+    Given user already logged in with "marketing1@cybertek.com" username "UserUser" password
+    When user click "Tasks" "All" under Activity stream
+    And user click created task
+    And user click EDIT
+    And user enter following informations
+      | Things to do | Test |
+    And user click "SAVE CHANGES" button
+    Then user should be able to edit the task
+  @wip
+  Scenario:Hr user can use edit functionality
+    Given user already logged in with "hr1@cybertek.com" username "UserUser" password
+    When user click "Tasks" "All" under Activity stream
+    And user click created task
+    And user click EDIT
+    And user click More
+    And user add Time tracking
+    And user add reminder
+    And user add Dependent tasks
+    And user click "SAVE CHANGES" button
+    Then user can see edited following informations
+      | Reminder        | alper@cybertekschool.com |
+      | Estimate        | 02:30:00                 |
+      | Dependent tasks | things to do             |
