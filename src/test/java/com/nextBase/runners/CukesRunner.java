03cf2cdea56cd0bfebcd74a896aaf66bead0f928
orkunncaglar <orkunncaglar@gmail.com>
2021-09-01 16:58:39 +0300

    update

--- src/test/java/com/nextBase/runners/CukesRunner.java
+++ src/test/java/com/nextBase/runners/CukesRunner.java
@@ -7,15 +7,19 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = {"json:target/cucumber.json",
+        plugin =  {"json:target/cucumber.json",
                 "html:target/default-html-reports"},
         features = "src/test/resources/features",
         glue = "com/nextBase/stepDefinitions",
-        dryRun = false,
-        tags = "@NEXTBASE-309"
+        dryRun = true
+        //tags =FailedTestRunner
 
 
 )
 
 public class CukesRunner {
+
+
+
+
 }
